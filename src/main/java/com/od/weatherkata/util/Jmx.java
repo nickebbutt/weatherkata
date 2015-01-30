54b20cda65e8e4f6307b74b8b9b27899d6b818c9
Nick Ebbutt <nick@ebbutt.net>
2015-01-30 17:48:54 +0000

    shut down jmx cleanly at termination

--- src/main/java/com/od/weatherkata/util/Jmx.java
+++ src/main/java/com/od/weatherkata/util/Jmx.java
@@ -6,8 +6,10 @@ import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 /**
  * Created by Nick E on 29/01/2015.
@@ -17,19 +19,27 @@ public class Jmx {
 
     public static void startJmx(int jmxPort) {
         try {
-            LocateRegistry.createRegistry(jmxPort);
 
             //        System.setProperty("com.sun.management.jmxremote.port","50123");
             //        System.setProperty("com.sun.management.jmxremote.authenticate","false");
             //        System.setProperty("com.sun.management.jmxremote.ssl","false");
+            Registry r = LocateRegistry.createRegistry(jmxPort);
             MBeanServer server = ManagementFactory.getPlatformMBeanServer();
 
-            JMXServiceURL jmxUrl
-                    = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + jmxPort + "/jmxrmi");
-            JMXConnectorServer connectorServer
-                    = JMXConnectorServerFactory.newJMXConnectorServer(jmxUrl, null, server);
+            JMXServiceURL jmxUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:" + jmxPort + "/jmxrmi");
+            JMXConnectorServer connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(jmxUrl, null, server);
 
             connectorServer.start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    //try to clean up on shutdown
+                    connectorServer.stop();
+                } catch (IOException e) {
+                    System.out.println("Shutting down JMX");
+                    e.printStackTrace();
+                }
+            }));
 
             System.setProperty(ChorusHandlerJmxExporter.JMX_EXPORTER_ENABLED_PROPERTY, "true");
 
